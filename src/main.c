d58bece02a154bcd705dc371f6683ee68a0a7d0b
crmaykish <crmaykish@gmail.com>
2018-08-18 15:57:28 -0400

    Building with ncurses

--- src/main.c
+++ src/main.c
@@ -1,32 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+#include <ncurses.h>
 #include "z80_cpu.h"
-#include "bitmath.h"
 
 int main(int argc, char const *argv[])
 {
-    FILE *f;
-    f = fopen(argv[1], "rb");
-
-    uint8_t rom_data[8192];
-
-    fread(rom_data, sizeof(rom_data), 1, f);
-
-    Z80_CPU cpu;
-
-    Z80_CPU_Init(&cpu);
-
-    Z80_CPU_SetMemory(&cpu, rom_data, sizeof(rom_data), 0);
-
-    printf("Hit <ENTER> to single-step through the ROM. Ctrl-C to exit.\n");
-
-    cpu.State = RUNNING;
-
-    while(cpu.State == RUNNING) {
-        getchar();
-        Z80_CPU_Cycle(&cpu);
-        Z80_CPU_PrintRegisters(&cpu);
-    }
-
-    return 0;
+    initscr();
+    printw("Z80 Debugger");
+    refresh();
+    getch();
+    endwin();
 }
